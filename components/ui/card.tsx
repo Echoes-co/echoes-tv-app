3d0a9d72a66faad09544bf6a8ccf6d448332f022
Jouan Jackson <jouanjackson@Jouans-Mac-mini.local>
2025-06-17 15:19:19 +0700

    Fix: add CardContent export

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -9,3 +9,14 @@ export function Card({
 }) {
   return <div className={className}>{children}</div>;
 }
+
+export function CardContent({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return <div className={`p-4 ${className || ""}`}>{children}</div>;
+}
+
